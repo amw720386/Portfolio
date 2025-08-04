446e49095a5adfa07ff5bfb194e66e9131a690cb
Ahamed Mohamed Wajibu <ahamedmohamedwajibu@gmail.com>
2025-08-03 22:56:04 -0400

    -_-

--- static/js/terminal.js
+++ static/js/terminal.js
@@ -1,6 +1,8 @@
 document.getElementById("terminal-input").addEventListener("keydown", function (e) {
     if (e.key === "Enter") {
       const value = this.value.trim().toLowerCase();
+
+      // YEEESSS I KNOW THIS IS UNSAFE STOOOP 
       if (value === "admin") {
         window.location.href = "https://api.ahamedwajibu.com/admin-login";  
       }
